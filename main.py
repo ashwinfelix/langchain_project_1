3d77dd1d896f4f7b24a614b3dfecc0a051444efa
ashwinfelix <ashwinfelixmanoj@gmail.com>
2026-08-07 00:41:45 +0530

    Tried Agent tools

---
+++ main.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+@tool
+def search(querry: str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+        querry: The querry to search for
+    Returns:
+        The search result
+    """
+    print(f"Searching for {querry}")
+    return "Tokyo weather is sunny"
+
+llm = ChatOpenAI()
+tools = [search]
+agent = create_agent(model=llm,tools=tools)
+
+def main():
+    result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokyo?")})
+    print(result)
+
+if __name__ == "__main__":
+    main()
