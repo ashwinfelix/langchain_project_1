b0c80056972568d56fafc184000a4d7020f864c4
ashwinfelix <ashwinfelixmanoj@gmail.com>
2026-08-09 01:10:39 +0530

    Added pydantic structure

--- src/langchain_project_1/__init__.py
+++ src/langchain_project_1/__init__.py
@@ -4,8 +4,23 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from tavily import TavilyClient
+from typing import List
+from pydantic import BaseModel, Field
 
 load_dotenv()
+
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url:str = Field(description='The url of the source')
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer:str = Field(description="The agent's answer to the querry")
+    sources: List[Source] = Field(default_factory=list, description="list of the sources used to generate the answer")
+
+
 
 tavily = TavilyClient()
 
@@ -23,7 +38,7 @@ def search(querry: str) -> str:
 
 llm = ChatOpenAI(model="gpt-5")
 tools = [search]
-agent = create_agent(model=llm,tools=tools)
+agent = create_agent(model=llm,tools=tools,response_format=AgentResponse)
 
 def main():
     result = agent.invoke({"messages":HumanMessage(content="Search for 3 job openings in south india for lanchain agntic AI developers for 1 year work experience individuals?")})
